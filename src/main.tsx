46061eb86bd6f9528cab5fdb59e70cd3cfb6161b
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-04 06:11:49 -0600

    Switch development branch to production homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./workflow.css";
+import ProductionHome from "./ProductionHome";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ProductionHome />
   </React.StrictMode>,
 );
