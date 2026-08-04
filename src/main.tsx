11bd39035129de856935f0adeff5000bf8433292
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-04 13:59:40 -0600

    Launch operating listings app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ProductionHome from "./ProductionHome";
+import OperatingApp from "./OperatingApp";
 import "./sprint14.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ProductionHome />
+    <OperatingApp />
   </React.StrictMode>,
 );
