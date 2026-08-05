eade83cdbc0f5bfa4acb6aae0765cff2ce9eb441
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-04 19:08:46 -0600

    Load token analyzer styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import OperatingApp from "./OperatingApp";
 import "./sprint14.css";
+import "./analyzer.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
