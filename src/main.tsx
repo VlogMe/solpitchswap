e8f1e64e717adf5ba9848db6232b6ec56cab01bf
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-05 07:19:06 -0600

    Import launch ready styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import OperatingApp from "./OperatingApp";
 import "./sprint14.css";
 import "./analyzer.css";
+import "./launch-ready.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
