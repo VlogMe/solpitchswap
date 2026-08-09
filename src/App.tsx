e2723b6f6907daa3e778d7f90386d276baf7c6ee
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-09 09:51:32 -0600

    Mount CA copied confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./detail-layout-fix.css";
 import "./project-action-buttons.css";
 import OperatingApp from "./OperatingApp";
 import SiteFooter from "./SiteFooter";
+import CopyCaToast from "./CopyCaToast";
 
 export default function App() {
   return (
     <>
       <OperatingApp />
+      <CopyCaToast />
       <SiteFooter />
     </>
   );
