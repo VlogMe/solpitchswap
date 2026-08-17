9de4e0c05ce5309e6eae9030c5241f9f2377e0e5
Vlogme <42412057+VlogMe@users.noreply.github.com>
2026-08-17 13:39:31 -0600

    Load nav listing policy copy

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import OperatingApp from "./OperatingApp";
 import JupiterSwapPortal from "./JupiterSwapPortal";
 import HeaderBrandPortal from "./HeaderBrandPortal";
 import PromotionOptionsPortal from "./PromotionOptionsPortal";
+import NavListingPolicyPortal from "./NavListingPolicyPortal";
 import "./sprint14.css";
 import "./analyzer.css";
 import "./launch-ready.css";
@@ -18,5 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <JupiterSwapPortal />
     <HeaderBrandPortal />
     <PromotionOptionsPortal />
+    <NavListingPolicyPortal />
   </React.StrictMode>,
 );
